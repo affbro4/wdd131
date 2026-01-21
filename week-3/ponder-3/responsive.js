a09bfb8cf159f62d05c2ca102126a2be6210d6d8
affbro4 <aff22001@byui.edu>
2026-01-20 20:50:52 -0700

    ponder done

---
+++ week-3/ponder-3/responsive.js
@@ -0,0 +1,9 @@
+
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+
+menuBtn.addEventListener('click', () => {
+   
+    nav.classList.toggle('active');
+});
