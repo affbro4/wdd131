3344722bf38d2f4bb4217f2e5275403748918fcb
affbro4 <aff22001@byui.edu>
2026-01-27 15:48:43 -0700

    homework

--- week-4/ponder-4/responsive.js
+++ week-4/ponder-4/responsive.js
@@ -1,10 +1,10 @@
-
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
+const trans = document.querySelector('.change')
 
 menuBtn.addEventListener('click', () => {
-   
+   menuBtn.classList.toggle('change')
    nav.classList.toggle('active');
+
 });
 
-window.addEventListener('resize',)
