4f171a6584096def0499a31bdbe5216161fd7c01
affbro4 <aff22001@byui.edu>
2026-01-27 16:21:46 -0700

    trying to do extra and failing

--- week-4/ponder-4/responsive.js
+++ week-4/ponder-4/responsive.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
-const trans = document.querySelector('.change')
+
 
 menuBtn.addEventListener('click', () => {
    menuBtn.classList.toggle('change')
