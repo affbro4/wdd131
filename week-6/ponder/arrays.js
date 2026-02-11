cf06a4ac00b1a36a5c518dd97f1cbf3542bbcc3e
affbro4 <aff22001@byui.edu>
2026-02-10 17:07:40 -0700

    messgae

---
+++ week-6/ponder/arrays.js
@@ -0,0 +1,59 @@
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+
+const students = [
+    {last: 'Andrus', first: 'Aaron', grade: 'A'},
+    {last: 'Masa', first:'Manny', grade: 'B'},
+    {last: 'Tanda', first: 'Tamanda', grade: 'C'},
+];
+          
+
+function convert(grade) {
+    switch (grade){
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+
+const displayList = document.querySelector("#student-list");
+
+students.forEach((student, index) => { 
+    const pointValue = convert(student.grade);
+    const li = document.createElement('li');
+    console.log(`[${index}] = ${student.first} ${student.last}: ${pointValue} points`);
+    li.textContent = `${student.first} ${student.last}: ${pointValue} points`;
+    
+    
+    displayList.appendChild(li);
+
+    
+    
+
+});
+
+
+const fruitElements = document.querySelectorAll("#fruit li");
+words.forEach((fruit, index) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = fruit;
+    document.querySelector("#fruit").appendChild(listItem);
+    console.log(`[${index}] = ${fruit}`)
+    
+});
